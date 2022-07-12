de0a8574e96b1cfa87e65aa92e898a93f91d58cd
Jonathan Moreno <johnfrom209@gmail.com>
2022-07-11 23:57:12 -0700

    Added some js to the application

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,35 @@
+
+
+
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
+for(n = 0; n <topics.length; n++){
+    console.log(topics[n]);
+}
+
+/*  if (topics === "HTML") 
+{
+    console.log("Let's study HTML!"); 
+}
+else if (topics === "CSS") 
+{
+    console.log("Let's study CSS!"); 
+}
+else if (topics === "Git") 
+{
+    console.log("Let's study Git!"); 
+}
+else if (topics === "JavaScript") 
+{
+    console.log("Let's study JavaScript!"); 
+}
+else{
+    console.log("Please try again!");
+}
+*/
+
+/*  var shapes = ["triangle", "square", "pentagon", "circle"];
+
+for(var x = 0; x <shapes.length; x++){
+    console.log(shapes[x]);
+}
+*/
